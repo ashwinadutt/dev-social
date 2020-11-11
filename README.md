eda193fc23c1c4936a11b1589b23d6ea1de04d35
Ashwin Dutt <ashwinadutt@gmail.com>
2020-11-11 21:05:36 +1100

    Initialising Git Repo

---
+++ README.md
@@ -0,0 +1,9 @@
+1: Install packages
+
+run `npm instll`
+
+2: Run project
+
+run `npm start`
+
+3: Open it [http://localhost:3000](http://localhost:3000)
---
+++ src/redux/reducers/dataReducer.js
@@ -0,0 +1,77 @@
+import {
+  SET_POSTS,
+  LIKE_POST,
+  UNLIKE_POST,
+  LOADING_DATA,
+  DELETE_POST,
+  POST_POST,
+  SET_POST,
+  SUBMIT_COMMENT
+} from "../types";
+
+const initialState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case LOADING_DATA:
+      return {
+        ...state,
+        loading: true
+      };
+    case SET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loading: false
+      };
+    case SET_POST:
+      return {
+        ...state,
+        post: action.payload
+      };
+    case LIKE_POST:
+    case UNLIKE_POST:
+      let index = state.posts.findIndex(
+        post => post.postId === action.payload.postId
+      );
+      state.posts[index] = action.payload;
+      if (state.post.postId === action.payload.postId) {
+        action.payload.comments = state.post.comments;
+        state.post = action.payload;
+      }
+      return {
+        ...state
+      };
+    case DELETE_POST:
+      index = state.posts.findIndex(post => post.postId === action.payload);
+      state.posts.splice(index, 1);
+      return {
+        ...state
+      };
+    case POST_POST:
+      return {
+        ...state,
+        posts: [action.payload, ...state.posts]
+      };
+    case SUBMIT_COMMENT:
+      return {
+        ...state,
+        post: {
+          commentCount: state.post.commentCount + 1,
+          createdAt: state.post.createdAt,
+          userHandle: state.post.userHandle,
+          userImage: state.post.userImage,
+          likeCount: state.post.likeCount,
+          body: state.post.body,
+          postId: state.post.postId,
+          comments: [action.payload, ...state.post.comments]
+        }
+      };
+    default:
+      return state;
+  }
+}
